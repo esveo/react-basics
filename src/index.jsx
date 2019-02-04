85835e4d93e8bd6e3972aa8c8b6104f2f4eb412d
Andreas Roth <art_github@rtline.de>
2019-02-04 12:43:20 +0100

    Remove commments

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 // #if >=02
 //   #if 02
-// `React.createElement` still needs to be imported, because `<Hello name="JSX" />` will be
+// `React` still needs to be imported, because `<Hello name="JSX" />` will be
 // transformed to `React.createElement(Hello, { name: "JSX" }`) by the transpiler.
 //   #endif
 import 'babel-polyfill'
@@ -11,14 +11,14 @@ import { render } from 'react-dom'
 import App from './App'
 
 //   #if 02
-/*
+
 function Hello(props) {
   return <p>Hello, {props.name}</p>
 }
 
 // The JSX syntax will be transformed to a regular `React.createElement` call.
 render(<Hello name="JSX" />, document.getElementById('app'))
-*/
+
 //   #else
 render(<App />, document.getElementById('app'))
 //   #endif
