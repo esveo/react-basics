dd5dc6a0e2e719a1a02b3c049bb47691852e7978
andreasroth <andreas.roth@xent.de>
2019-02-25 10:45:53 +0100

    Adapt solutions infrastructure

--- src/Solutions.js
+++ src/Solutions.js
@@ -1,7 +1,12 @@
+import styled from '@emotion/styled/macro';
+import { faAngleRight, faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { allSolutions } from './sections/allSolutions';
+import { dash } from './__helpers/dash';
+import { Link, NavLink } from './__helpers/slides/Link';
 
 export function Solutions() {
   return (
@@ -13,19 +18,34 @@ export function Solutions() {
               path={match.path}
               exact
               render={() => (
-                <>
-                  <h1>Solutions</h1>
-                  <ul>
-                    {allSolutions.map((solution, index) => (
-                      <li key={index}>
-                        <Link to={match.url + '/' + index}>
-                          {index + 1}. {solution.name}
-                        </Link>
-                        - <span>{solution.fileSystemPath}</span>
-                      </li>
-                    ))}
-                  </ul>
-                </>
+                <Header
+                  breadcrumbs={[{ path: '/solutions', name: 'Solutions' }]}
+                >
+                  <SolutionsContainer>
+                    <h1>Solutions</h1>
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Result</th>
+                          <th>Source Code</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {allSolutions.map((solution, index) => (
+                          <tr key={index}>
+                            <td>
+                              <Link to={match.url + '/' + index}>
+                                {index + 1}. {solution.name}
+                              </Link>
+                            </td>
+                            <td>{solution.fileSystemPath}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                    <ul />
+                  </SolutionsContainer>
+                </Header>
               )}
             />
             <Switch>
@@ -33,7 +53,22 @@ export function Solutions() {
                 <Route
                   key={index}
                   path={match.path + '/' + index}
-                  render={() => <Solution solution={solution} />}
+                  render={() => (
+                    <Header
+                      breadcrumbs={[
+                        { path: '/solutions', name: 'Solutions' },
+                        {
+                          path: match.path + '/' + index,
+                          name:
+                            solution.name +
+                            ` ${dash} ` +
+                            solution.fileSystemPath
+                        }
+                      ]}
+                    >
+                      <Solution solution={solution} />
+                    </Header>
+                  )}
                 />
               ))}
             </Switch>
@@ -43,6 +78,25 @@ export function Solutions() {
     </>
   );
 }
+
+const SolutionsContainer = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.4em;
+
+  thead {
+    font-weight: bold;
+  }
+
+  td,
+  th {
+    padding: 5px 20px;
+    text-align: left;
+  }
+`;
 
 function Solution({ solution }) {
   const containerParentRef = React.useRef();
@@ -63,3 +117,61 @@ function Solution({ solution }) {
 
   return <div ref={containerParentRef} />;
 }
+
+function Header({ children, breadcrumbs = [] }) {
+  return (
+    <HeaderContainer>
+      <HeaderNav>
+        <Link to="/">
+          <FontAwesomeIcon icon={faHome} />
+        </Link>
+        {breadcrumbs.map(({ path, name }) => (
+          <React.Fragment key={name}>
+            <Divider>
+              <FontAwesomeIcon icon={faAngleRight} />
+            </Divider>
+            <NavLink to={path} activeClassName="active" exact>
+              {name}
+            </NavLink>
+          </React.Fragment>
+        ))}
+      </HeaderNav>
+      <HeaderChildrenContainer>{children}</HeaderChildrenContainer>
+    </HeaderContainer>
+  );
+}
+
+const Divider = styled.span`
+  color: white;
+`;
+
+const HeaderContainer = styled.div`
+  height: 100%;
+  overflow: hidden;
+`;
+
+const HeaderNav = styled.div`
+  align-items: center;
+  display: flex;
+  font-size: 1.2em;
+  height: 4em;
+  background: rgb(35, 35, 35);
+
+  & > * {
+    margin: 10px;
+  }
+
+  a {
+    color: white;
+    text-decoration: underline;
+  }
+
+  ${NavLink}.active {
+    text-decoration: none;
+  }
+`;
+
+const HeaderChildrenContainer = styled.div`
+  height: calc(100% - 4em);
+  overflow: auto;
+`;
--- src/__helpers/slides/Link.js
+++ src/__helpers/slides/Link.js
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled/macro';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, NavLink as RouterNavLink } from 'react-router-dom';
 
 export const Link = styled(RouterLink)`
   color: rgb(63, 167, 254);
   text-decoration: none;
 `;
+
+export const NavLink = Link.withComponent(RouterNavLink);
