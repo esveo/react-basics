b34c5c0a280b5d31b86fb6ad13f45456263d87b1
andreasroth <andreas.roth@xent.de>
2019-04-30 10:14:23 +0200

    Cache last inmemory route in local storage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { MemoryRouter as Router, Route } from 'react-router-dom';
+import React, { Component, useEffect } from 'react';
+import { MemoryRouter as Router, Route, withRouter } from 'react-router-dom';
 import './App.css';
 import { Exercises } from './Exercises';
 import { Home } from './Home';
@@ -8,12 +8,9 @@ import { Slides } from './Slides';
 class App extends Component {
   render() {
     return (
-      <Router
-        initialEntries={
-          process.env.NODE_ENV === 'production' ? undefined : ['/slides']
-        }
-      >
+      <Router initialEntries={getInitialEntries()}>
         <>
+          <StoreLastPath />
           <Route path="/" exact render={() => <Home />} />
           <Route path="/slides" render={() => <Slides />} />
           <Route path="/exercises" render={() => <Exercises />} />
@@ -24,3 +21,24 @@ class App extends Component {
 }
 
 export default App;
+
+const localStorageRouteKey = 'react-basic-training--location';
+function setLocation(location) {
+  try {
+    localStorage.setItem(localStorageRouteKey, location);
+  } catch (err) {}
+}
+function getInitialEntries() {
+  try {
+    const last = localStorage.getItem(localStorageRouteKey);
+    return last ? [last] : undefined;
+  } catch (err) {
+    return undefined;
+  }
+}
+const StoreLastPath = withRouter(function StoreLastPath(props) {
+  useEffect(() => {
+    setLocation(props.location.pathname);
+  }, [props.location.pathname]);
+  return null;
+});
