a796972ec025e7fee5c6de61ac537c1f7180ab4e
andreasroth <andreas.roth@xent.de>
2019-04-30 10:45:11 +0200

    Properly center footer

--- src/__helpers/slides/Footer.js
+++ src/__helpers/slides/Footer.js
@@ -1,7 +1,7 @@
-import PropTypes from 'prop-types'
 import styled from '@emotion/styled/macro';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PropTypes from 'prop-types';
 import React from 'react';
 import { Link } from './Link';
 import { Logo } from './Logo';
@@ -20,17 +20,17 @@ export function Footer() {
 
 class SlideNumber extends React.Component {
   static contextTypes = {
-    slideHash: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    slideHash: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
   };
   render() {
-    return <SlideNumberText>{this.context.slideHash}</SlideNumberText>
+    return <SlideNumberText>{this.context.slideHash}</SlideNumberText>;
   }
 }
 
 const SlideNumberText = styled('span')`
   font-size: 0.8em;
   font-variant-numeric: tabular-nums;
-`
+`;
 
 const FooterContainer = styled('div')`
   align-items: center;
@@ -47,5 +47,16 @@ const FooterContainer = styled('div')`
 
   ${Link} {
     font-size: 1.2em;
+    flex: 1 1 0;
+    text-align: left;
+  }
+
+  ${SlideNumberText} {
+    flex: 1 1 0;
+  }
+
+  a {
+    flex: 1 1 0;
+    text-align: right;
   }
 `;
