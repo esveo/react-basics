c0020709c0291f9c2c717afe48158a35dc064a1b
Andreas Roth <art_github@rtline.de>
2019-09-17 08:34:07 +0200

    Add backup fonts

--- src/__helpers/slides/Heading.js
+++ src/__helpers/slides/Heading.js
@@ -3,7 +3,9 @@ import { Heading as SpectacleHeading } from 'spectacle';
 
 export const Heading = styled(SpectacleHeading)`
   && {
-    font-family: 'Klavika';
+    font-family: 'Klavika', 'Helvetica', -apple-system, BlinkMacSystemFont,
+      'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+      'Droid Sans', 'Helvetica Neue', sans-serif !important;
     margin-top: 1em;
     font-size: ${({ size = 1 }) => [2.6, 2, 1.8, 1.4, 1.2, 1][size - 1]}em;
   }
--- src/__helpers/slides/Text.js
+++ src/__helpers/slides/Text.js
@@ -3,7 +3,9 @@ import { Text as SpectacleText } from 'spectacle';
 
 export const Text = styled(SpectacleText)`
   && {
-    font-family: 'Klavika';
+    font-family: 'Klavika', 'Helvetica', -apple-system, BlinkMacSystemFont,
+      'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+      'Droid Sans', 'Helvetica Neue', sans-serif !important;
     margin-bottom: 1em;
     font-size: ${p => p.size || 1}em;
     text-align: ${p => (p.left ? 'left' : 'center')};
